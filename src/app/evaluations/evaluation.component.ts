c1299f0676428aa9c527e864ed2093c9fe1b2da0
Guilherme Nass <Guilherme.Nass@dinamio.com.br>
2018-02-26 13:22:37 -0300

    correção problema do datepicker

--- src/app/evaluations/evaluation.component.ts
+++ src/app/evaluations/evaluation.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+
 declare const $: any;
 declare var Morris: any;
 
 @Component({
   selector: 'app-evaluation',
-  templateUrl: './evaluation.component.html',
-  styleUrls: ['./evaluation.component.css']
+  templateUrl: './evaluation.component.html'
 })
 
 export class EvaluationComponent implements OnInit {
