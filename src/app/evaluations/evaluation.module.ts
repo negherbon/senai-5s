5d29dd6935afb89fb79374199f90a55404652743
Guilherme Nass <nassguilherme@gmail.com>
2018-05-10 20:53:34 -0300

    ajuste no module da evaluation

--- src/app/evaluations/evaluation.module.ts
+++ src/app/evaluations/evaluation.module.ts
@@ -5,14 +5,12 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { EvaluationComponent } from './evaluation.component';
 import { EvaluationRoutes } from './evaluation.routing';
 import { SharedModule } from '../shared/shared.module';
-import { AppMaskerModule } from 'brmasker';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(EvaluationRoutes),
     SharedModule,
-    AppMaskerModule,
     BsDatepickerModule.forRoot()
   ],
   declarations: [EvaluationComponent]
