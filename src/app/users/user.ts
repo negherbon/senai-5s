aeb126d815970b29771f891fec3816564a02fc51
Guilherme Nass <nassguilherme@gmail.com>
2018-06-12 21:36:11 -0300

    alterações p/ andré testar

--- src/app/users/user.ts
+++ src/app/users/user.ts
@@ -1,8 +1,17 @@
 export class User {
-    id: number;
-    name: string;
-    email: string;
-    password: string;
-    userName: string;
-    profile: string;
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  userName: string;
+  profile: string;
+
+  constructor(id?: number, name?: string, email?: string, password?: string, userName?: string, profile?: string){
+    this.id = id
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.userName = userName;
+    this.profile = profile;
   }
+}
